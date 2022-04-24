cbece4e0c50bd7a15a91e22c7d18c1df0655e918
David <david.landais44@gmail.com>
2022-04-24 13:19:48 +0000

    add health route and vscode gitpod extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import { response } from 'express';
 
+const startTime = Date.now();
 const app = express();
+
+app.get('/', (req, res) => {
+    const elaspedTime = Date.now() - startTime;
+    return res.json({
+        alive: true,
+        startTime,
+        elaspedTime,
+    });
+});
 
 app.get('/movies', async (request, response) => {
     // Je souhaite récupérer tous les films
@@ -23,4 +34,4 @@ app.get('/movies/:title', async (request, response) => {
     // Je souhaite supprimer un film par son titre
 // });
 
-app.listen(8080);
+app.listen(8080, '0.0.0.0', () => console.log('Server is running...'));
